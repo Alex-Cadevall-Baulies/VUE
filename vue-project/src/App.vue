62733063a2bd5c80ccd9328c1b50bc32150059d4
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-29 12:51:51 +0100

    several tests and props added

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,46 @@
 <template>
-  <h1> {{ props }}</h1>
+  <!-- we can also send props to the link components -->
+  <!-- those props can be taken from the data section inside export -->
+  <Modal :header='header' text="some text"/>
+  <br>
+  <h1> {{about}} </h1>
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click here</button>
+  <br>
   <ul v-for=" cover in avrilCovers">
-      <a :href="cover.url"><img :src="cover.image" :alt="cover.name"></a>
+    <a :href="cover.url"><img :src="cover.image" :alt="cover.name"></a>
+    <br>
   </ul>
 </template>
 
 <script>
+//we add the child components in this way
+import Modal from './components/Modal.vue'
+
 export default {
+  //I register it inside
+  components: { Modal },
   data() {
       return {
-          props: "Vue JS 3 tutorial from the net ninja for full tutorial",
+          about: "Vue JS 3 tutorial from the net ninja for full tutorial",
           avrilCovers: [
               {url: 'https://www.youtube.com/watch?v=Bg59q4puhmg', image: "../src/assets/images/av1.jpg", name: "girlfriend"},
               {url: 'https://www.youtube.com/watch?v=5NPBIwQyPWE', image: "../src/assets/images/av2.jpg", name: "complicated"},
-          ]
+          ],
+          header: "This is a header",
       }
   },
+
+  methods: {
+    handleClick() {
+      //We can access the refs in the following way:
+      console.log(this.$refs.name)
+      //we can add a class to the element
+      this.$refs.name.classList.add('active')
+      //other examples
+      this.$refs.name.focus()
+    }
+  }
 }
 </script>
---
+++ vue-project/src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>This is a modal</h1>
+            <p>{{ header }}</p>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+// we are accepting the prop sent from App.vue
+export default {
+    props: ['header', 'text']
+}
+</script>
+
+<!-- if we used scoped in a style we limit the use of it to this component -->
+<!-- we can also limit it by making sure it only affects the desired class/ref as done in .modal h1 -->
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: auto;
+        margin: 100px auto;
+        background: red;
+        border-radius: 20px;
+        text-align: center;
+    }
+    .modal h1 {
+        color: white;
+        border: none;
+        padding: 0;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: black (0,0,0,0.5);
+        width: 100%;
+        height: 100%
+    }
+</style>
