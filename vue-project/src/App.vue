bb36c02461e7ac97de900d3fad357d10f2c34550
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-29 11:07:57 +0100

    ES module start

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1> {{ props }}</h1>
+  <ul v-for=" cover in avrilCovers">
+      <a :href="cover.url"><img :src="cover.image" :alt="cover.name"></a>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  data() {
+      return {
+          props: "Vue JS 3 tutorial from the net ninja for full tutorial",
+          avrilCovers: [
+              {url: 'https://www.youtube.com/watch?v=Bg59q4puhmg', image: "../src/assets/images/av1.jpg", name: "girlfriend"},
+              {url: 'https://www.youtube.com/watch?v=5NPBIwQyPWE', image: "../src/assets/images/av2.jpg", name: "complicated"},
+          ]
+      }
+  },
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
