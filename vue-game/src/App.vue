f221ece87daf1d0d351872911e8d465b4e134147
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-30 12:47:23 +0100

    settingup game

---
+++ vue-game/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>{{ title }}</h1>
+<!--  disabled property makes button unclickable if game = true -->
+<button @click="toggleGame" :disabled="game"> Play Game </button>
+<Block v-if="game" :delay='delay'/>
+</template>
+
+<script>
+//import Result from './components/Results.vue'
+import Block from './components/Block.vue'
+
+export default {
+  components: { Block },
+  data() {
+    return{
+    title: 'Vue Game',
+    game: false,
+    delay: null
+    }
+  },
+  methods: {
+    toggleGame() {
+      //give me a time between 2000 and 7000 ms
+      this.delay = 2000 + Math.random() * 5000 
+      this.game = !this.game
+      console.log(this.delay)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  text-align: center;
+  color: #639;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-game/src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="block">
+        I'm a block
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['delay']
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    background: #639;
+    color: antiquewhite;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
