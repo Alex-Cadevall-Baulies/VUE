aac419537a0e26ba96a76eadfe134c8c5e905932
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-30 12:01:06 +0100

    basic vue done

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <!-- we can also send props to the link components -->
-  <!-- those props can be taken from the data section inside export -->
-  <Modal :header='header' text="some text"/>
-  <br>
-  <h1> {{about}} </h1>
-  <br>
-  <input type="text" ref="name">
-  <button @click="handleClick">Click here</button>
-  <br>
-  <ul v-for=" cover in avrilCovers">
-    <a :href="cover.url"><img :src="cover.image" :alt="cover.name"></a>
-    <br>
-  </ul>
+  <div>
+    <h1> {{ about }} </h1>
+
+    <div>
+      <input type="text" ref="name">
+      <button @click="handleClick">Click here</button>
+    </div>
+    <div>
+      <ul v-for=" cover in avrilCovers">
+        <a :href="cover.url"><img :src="cover.image" :alt="cover.name"></a>
+      </ul>
+    </div>
+    <div>
+      <p>There is a modal here</p>
+      <button @click="toggleModal">Open Modal</button>
+    </div>
+  </div>
+    <!-- if true we will show what's written in Modal.vue -->
+    <!-- we have also changed divs to teleport to activate the teleport fucntion on index.html -->
+    <Teleport to=".modals" v-if="showModal">
+      <!-- we can also send props to the link components -->
+      <!-- those props can be taken from the data section inside export -->
+      <!-- theme prop is now a boolean, this will change things on modal.vue depending on if true or false -->
+      <!-- @close fires method close from Modal.vue -->
+      <Modal :header='header' text="some text" :theme='theme' @close="toggleModal">
+        <!-- we can also send teamplates like this (called slots) -->
+        <h1>This is a Slot!</h1>
+        <p>look at me I'm a Slot!</p>
+        <!-- we can name slots using teamplates -->
+        <template v-slot:links>
+          <a href="https://www.youtube.com/watch?v=mOYZaiDZ7BM">click here for more music</a> <br>
+          <a href="https://www.youtube.com/watch?v=LBQ2305fLeA">or here for even more music</a>
+        </template>
+      </Modal>
+    </Teleport>
 </template>
 
 <script>
@@ -22,14 +44,16 @@ export default {
   //I register it inside
   components: { Modal },
   data() {
-      return {
-          about: "Vue JS 3 tutorial from the net ninja for full tutorial",
-          avrilCovers: [
-              {url: 'https://www.youtube.com/watch?v=Bg59q4puhmg', image: "../src/assets/images/av1.jpg", name: "girlfriend"},
-              {url: 'https://www.youtube.com/watch?v=5NPBIwQyPWE', image: "../src/assets/images/av2.jpg", name: "complicated"},
-          ],
-          header: "This is a header",
-      }
+    return {
+      about: "Vue JS 3 tutorial from the net ninja for full tutorial",
+      avrilCovers: [
+        { url: 'https://www.youtube.com/watch?v=Bg59q4puhmg', image: "../src/assets/images/av1.jpg", name: "girlfriend" },
+        { url: 'https://www.youtube.com/watch?v=5NPBIwQyPWE', image: "../src/assets/images/av2.jpg", name: "complicated" },
+      ],
+      header: "This is a header",
+      theme: "sale",
+      showModal: false
+    }
   },
 
   methods: {
@@ -40,6 +64,11 @@ export default {
       this.$refs.name.classList.add('active')
       //other examples
       this.$refs.name.focus()
+    },
+
+    // we use it to toggle on and of modal
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
--- vue-project/src/components/Modal.vue
+++ vue-project/src/components/Modal.vue
@@ -1,9 +1,18 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
+    <!-- @click.shift informs that only while pressing outside of the modal it will close -->
+    <div class="backdrop" @click.self="closeModal">
+        <!-- we can send several classes, the second one acts as a boolean -->
+        <div class="modal" :class="{ sale: theme === 'sale' }">
             <h1>This is a modal</h1>
             <p>{{ header }}</p>
             <p>{{ text }}</p>
+        </div>
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <!-- we are sending the app.vue slots -->
+            <slot></slot>
+            <div class="hyperlink">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -11,31 +20,50 @@
 <script>
 // we are accepting the prop sent from App.vue
 export default {
-    props: ['header', 'text']
+    props: ['header', 'text', 'theme'],
+    methods: {
+        // this is a custom event, fires here and affects parent component (app.js)
+        closeModal() {
+            //$emit allows us to emit an event, in this case we call "close" to our event
+            this.$emit('close')
+        }
+    } 
 }
 </script>
 
 <!-- if we used scoped in a style we limit the use of it to this component -->
-<!-- we can also limit it by making sure it only affects the desired class/ref as done in .modal h1 -->
 <style scoped>
-    .modal {
-        width: 400px;
-        padding: auto;
-        margin: 100px auto;
-        background: red;
-        border-radius: 20px;
-        text-align: center;
-    }
-    .modal h1 {
-        color: white;
-        border: none;
-        padding: 0;
-    }
-    .backdrop {
-        top: 0;
-        position: fixed;
-        background: black (0,0,0,0.5);
-        width: 100%;
-        height: 100%
-    }
+.modal {
+    width: 400px;
+    padding: auto;
+    margin: 100px auto;
+    background: red;
+    border-radius: 20px;
+    text-align: center;
+}
+
+/* we can also limit it by making sure it only affects the desired class/ref as done in .modal h1 */
+.modal h1 {
+    color: white;
+    border: none;
+    padding: 0;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: black;
+    width: 100%;
+    height: 100%
+}
+
+/* modal div colors will change depending of Theme props, if = to sale this will activate it */
+.modal.sale {
+    background-color: crimson;
+    color: aliceblue
+}
+
+.modal.sale.h1 {
+    color: aliceblue
+}
 </style>
